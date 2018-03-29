c1097341b8f926eff58809c96f851c2a2312a3b1
Martin Perren <martinperren@gmail.com>
2018-03-29 18:29:28 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,14 @@ client.on('message', message => {
   	}
 });
 
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
