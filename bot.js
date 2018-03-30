ec1edfdbf2625e4aecc679a6f641db6ab145f6d4
Martin Perren <martinperren@gmail.com>
2018-03-30 04:13:56 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,11 +40,11 @@ client.on("message", async message => {
   
   // Let's go with a few common example commands! Feel free to delete or change those.
   
-  if(command === "ping") {
+    if(command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Tu ping es ${m.createdTimestamp - message.createdTimestamp}ms. API ping: ${Math.round(client.ping)}ms`);
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
   
   if(command === "say") {
@@ -61,21 +61,21 @@ client.on("message", async message => {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
     // Please read on Array.some() to understand this bit: 
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["OWNER","ADMIN", "MOD"].includes(r.name)) )
-      return message.reply("no tienes permisos para usar este comando");
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
     
     // Let's first check if we have a member and if we can kick them!
     // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
     let member = message.mentions.members.first();
     if(!member)
-      return message.reply("menciona a un miembro con @");
+      return message.reply("Please mention a valid member of this server");
     if(!member.kickable) 
-      return message.reply("no puedo kickear a este usuario.");
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
     
     // slice(1) removes the first part, which here should be the user mention!
     let reason = args.slice(1).join(' ');
     if(!reason)
-      return message.reply("indica la razon luego del nombre.");
+      return message.reply("Please indicate a reason for the kick!");
     
     // Now, time for a swift kick in the nuts!
     await member.kick(reason)
@@ -87,25 +87,25 @@ client.on("message", async message => {
   if(command === "ban") {
     // Most of this command is identical to kick, except that here we'll only let admins do it.
     // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["OWNER","ADMIN"].includes(r.name)) )
-      return message.reply("no tienes permisos para usar este comando");
+    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
     
     let member = message.mentions.members.first();
     if(!member)
-      return message.reply("menciona a un miembro con @");
+      return message.reply("Please mention a valid member of this server");
     if(!member.bannable) 
-      return message.reply("no puedo banear a este usuario.");
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
 
     let reason = args.slice(1).join(' ');
     if(!reason)
-      return message.reply("indica la razon luego del nombre.");
+      return message.reply("Please indicate a reason for the ban!");
     
     await member.ban(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
   }
   
-  if(command === "cc") {
+  if(command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
     
     // get the delete count, as an actual number.
@@ -113,18 +113,12 @@ client.on("message", async message => {
     
     // Ooooh nice, combined conditions. <3
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("coloca un numero entre 2 y 200 de lineas para borrar.");
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
     // So we get our messages, and delete them. Simple enough, right?
     const fetched = await message.channel.fetchMessages({count: deleteCount});
     message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`No puedo borrar los mensajes por: ${error}`));
-  }
-  
-  
-  if(command === "music") {
-const music = require('discord.js-music');
-music(client);
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
 });
 
