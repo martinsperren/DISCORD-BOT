4f74a027dd149a972a9a2c03cf6a3a30a67b0feb
Martin Perren <martinperren@gmail.com>
2018-03-29 23:02:40 -0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,182 +1,126 @@
-var Discord = require('discord.js');
-var Bot = new Discord.Client();
-var Helper = require('./components/helper.js');
-var Queue = require('./components/queue.js');
-var TrackHelper = require('./components/trackhelper.js');
-var WordService = require('./components/wordservice.js');
-var WeatherService = require('./components/weatherservice.js');
+// Load up the discord.js library
+const Discord = require("discord.js");
 
-var commands = {
-  '!video': {
-    execute: getVideo,
-    description: 'get a youtube video by search word'
-  },
-  '!weather': {
-    execute: getWeather,
-    description: 'get current weather for the given city, defaults to Stockholm'
-  },
-  '!roll': {
-    execute: roll,
-    description: 'roll from 1-100'
-  },
-  '!help': {
-    execute: showHelp
-  },
-  '!words': {
-    execute: countWordsByUser,
-    description: 'get the most popular words for user of the given username, defaults to your username'
-  },
-  '!queue': {
-    execute: doQueue,
-    description: 'queue your song'
-  },
-  '!voteskip': {
-    execute: voteSkip,
-    description: 'vote to skip the current song'
-  },
-  '!song': {
-    execute: showSong,
-    description: 'get the current song'
+// This is your client. Some people call it `bot`, some people call it `self`, 
+// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
+// this is what we're refering to. Your client.
+const client = new Discord.Client();
+
+// Here we load the config.json file that contains our token and our prefix values. 
+
+// config.token contains the bot's token
+// config.prefix contains the message prefix.
+
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+
+client.on("message", async message => {
+ 
+  const args = message.content.slice("!".length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  // Let's go with a few common example commands! Feel free to delete or change those.
+  
+  if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Tu ping es ${m.createdTimestamp - message.createdTimestamp}ms. API ping: ${Math.round(client.ping)}ms`);
   }
-};
+  
+  if(command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
+  
+  if(command === "kick") {
+    // This command must be limited to mods and admins. In this example we just hardcode the role names.
+    // Please read on Array.some() to understand this bit: 
+    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+    if(!message.member.roles.some(r=>["OWNER","ADMIN", "MOD"].includes(r.name)) )
+      return message.reply("no tienes permisos para usar este comando");
+    
+    // Let's first check if we have a member and if we can kick them!
+    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("menciona a un miembro con @");
+    if(!member.kickable) 
+      return message.reply("no puedo kickear a este usuario.");
+    
+    // slice(1) removes the first part, which here should be the user mention!
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("indica la razon luego del nombre.");
+    
+    // Now, time for a swift kick in the nuts!
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
 
-Bot.on('message', message => {
-  WordService.registerMessage(message);
+  }
+  
+  if(command === "ban") {
+    // Most of this command is identical to kick, except that here we'll only let admins do it.
+    // In the real world mods could ban too, but this is just an example, right? ;)
+    if(!message.member.roles.some(r=>["OWNER","ADMIN"].includes(r.name)) )
+      return message.reply("no tienes permisos para usar este comando");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("menciona a un miembro con @");
+    if(!member.bannable) 
+      return message.reply("no puedo banear a este usuario.");
 
-  if (isBotCommand(message)) {
-    execute(message.content, message);
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("indica la razon luego del nombre.");
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  }
+  
+  if(command === "cc") {
+    // This command removes all messages from all users in the channel, up to 100.
+    
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
+    
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("coloca un numero entre 2 y 200 de lineas para borrar.");
+    
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`No puedo borrar los mensajes por: ${error}`));
   }
 });
 
-function showSong(args, message) {
-  Queue.showSong(message);
-}
 
-function voteSkip(args, message) {
-  Queue.voteSkip(message);
-}
+client.login(process.env.BOT_TOKEN);
 
-function doQueue(args, message) {
-  if (args.length <= 0) {
-    return message.reply(Helper.wrap('Type of music need to be specified.'));
-  }
-
-  if (Queue.isFull()) {
-    return message.reply(Helper.wrap('Queue is full.'));
-  }
-
-  if (args.startsWith('http')) {
-    TrackHelper.getVideoFromUrl(args).then(track => {
-      Queue.add(track, message);
-    }).catch(err => {
-      message.reply(Helper.wrap(err));
-    });
-  } else {
-    TrackHelper.getRandomTrack(args, 5).then(track => {
-      Queue.add(track, message);
-    }).catch(err => {
-      message.reply(Helper.wrap(err));
-    });
-  }
-}
-
-function getVideo(args, message) {
-  TrackHelper.getRandomTrack(args, 5).then(track => {
-    message.reply(track.url);
-  }).catch(err => {
-    message.reply(Helper.wrap(err));
-  });
-}
-
-function countWordsByUser(args, message) {
-  WordService.countWordsByUser(args, message);
-}
-
-function getWeather(args, message) {
-  WeatherService.getWeather(args, message);
-}
-
-function showHelp(args, message) {
-  var toReturn = 'No commands to run!';
-  if (Object.keys(commands).length > 1) {
-    var toReturn = 'Available commands:\n';
-    for (var command in commands) {
-      if (command != '!help') {
-        data = commands[command];
-        toReturn += command + ': ' + data.description + getAvailableCommandAsText(data) + '\n';
-      }
-    }
-  }
-  message.reply(Helper.wrap(toReturn));
-}
-
-function getAvailableCommandAsText(command) {
-  if (!Helper.commandIsAvailable(command)) return ' (not available)';
-
-  return '';
-}
-
-function roll(content, message) {
-  message.reply(Helper.wrap('You rolled ' + getRandomNumber(1, 100) + ' (1-100)'));
-}
-
-function isBotCommand(message) {
-  if (message.content.startsWith('!') && message.author.id != Bot.user.id) {
-    return true;
-  }
-
-  return false;
-}
-
-function execute(content, message) {
-  var args = content.split(" ");
-  var command = commands[args[0]];
-  if (command) executeCommand(command, message, args);
-}
-
-function executeCommand(command, message, args) {
-  if (!Helper.commandIsAvailable(command)) {
-    return message.reply(Helper.wrap('Command is not available.'));
-  }
-
-  command.execute(getCommandArguments(args), message);
-}
-
-function getCommandArguments(args) {
-  var withoutCommand = args.slice(1);
-
-  return withoutCommand.join(" ");
-}
-
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function registerService(service, affectedCommands) {
-  service = new service();
-
-  if (affectedCommands) {
-    affectedCommands.forEach(command => {
-      var c = commands[command];
-      if (c) {
-        if (!c.services) c.services = [];
-        c.services.push(service);
-      }
-    });
-  }
-
-  return service;
-}
-
-function init() {
-  Helper.keys('apikeys', ['discord']).then(keys => {
-    Bot.login(keys.discord);
-
-    Queue = registerService(Queue, ['!queue', '!voteskip', '!song']);
-    TrackHelper = registerService(TrackHelper, ['!queue', '!video']);
-    WordService = registerService(WordService, ['!words']);
-    WeatherService = registerService(WeatherService, ['!weather']);
-  }).catch(console.error);
-}
-
-init();
