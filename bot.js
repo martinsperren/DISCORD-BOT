d51734b6f5f184da3e87be76aab3a991f2bb77ca
Martin Perren <martinperren@gmail.com>
2018-04-02 05:20:31 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
+
 // Load up the discord.js library
 const Discord = require("discord.js");
+const client = new Discord.Client();
 const stream = require('youtube-audio-stream')
 const ytdl = require('ytdl-core');
-const {
-  YTSearcher
-} = require('ytsearcher');
+const YTSearcher = require('ytsearcher');
 const ypi = require('youtube-playlist-info');
 
 
 // This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
-const client = new Discord.Client();
+
 
 // Here we load the config.json file that contains our token and our prefix values. 
 
