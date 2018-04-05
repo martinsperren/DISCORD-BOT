92f44ac1db4107fd4ab5526a75659ea04be52d23
Martin Perren <martinperren@gmail.com>
2018-04-05 15:48:40 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -217,11 +217,20 @@ member.removeRole('429091253129576448');
 	  
   }
   
- if(command === "music") {
+ if(command === "play") {
 
-const channel = message.member.voiceChannel;
+    const { voiceChannel } = message.member;
 
-    client.channel.join();
+        if (!voiceChannel) {
+            return message.reply('please join a voice channel first!');
+        }
+
+        voiceChannel.join().then(connection => {
+            const stream = ytdl('https://www.youtube.com/watch?v=D57Y1PruTlw', { filter: 'audioonly' });
+            const dispatcher = connection.playStream(stream);
+
+            dispatcher.on('end', () => voiceChannel.leave());
+        });
 	    
 	return message.reply("OK"); 
 	
