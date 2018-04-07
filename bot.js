49d5feb155aa5eec9676078c6c612246566da438
Martin Perren <martinperren@gmail.com>
2018-04-07 04:04:40 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ const queue = new Map();
 
 
 
-
+//xd
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
