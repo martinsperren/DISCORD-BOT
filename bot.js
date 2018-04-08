aecbf8ef09ec436de022e12b80b130fd0144fa39
Martin Perren <martinperren@gmail.com>
2018-04-07 23:07:12 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,15 +18,15 @@ const queue = new Map();
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setGame(`DE RUTA EN ${client.guilds.size} SERVERS`);
+  client.user.setGame(`DE RUTA CON ${Discord.client.guilds.get(219256995574710272).members.size()} PONIS`);
 });
 client.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setGame(`DE RUTA EN ${client.guilds.size} SERVERS`);
+  client.user.setGame(`DE RUTA CON ${Discord.client.guilds.get(219256995574710272).members.size()} PONIS`);
 });
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setGame(`DE RUTA EN ${client.guilds.size} SERVERS`);
+  client.user.setGame(`DE RUTA CON ${Discord.client.guilds.get(219256995574710272).members.size()} PONIS`);
 });
 client.on('guildMemberAdd', member => {
     member.guild.channels.get('219256995574710272').send('**' + member.user.username + '**, ahora vive en el bunker! :house:'); 
