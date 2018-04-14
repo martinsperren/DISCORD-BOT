0257d5b8c01ea1a4e472438a45974549fa0cd6ba
Martin Perren <martinperren@gmail.com>
2018-04-14 05:41:45 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const restClient = new Client();
 const configFile = "config.json";
 const fs = require('fs');
 
- OFFLINE SACAR COMENTARIO
+//OFFLINE SACAR COMENTARIO
 
 const Util = require('discord.js');
 const YouTube = require('simple-youtube-api');
