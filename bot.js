c96707f3967db4a51ee62e5c5e79b27b1ab3e46f
Martin Perren <martinperren@gmail.com>
2018-04-14 05:29:33 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -513,8 +513,8 @@ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
 	 }
 	
 	if (message.content.startsWith("!miembros")){
-	var memberCount = client.guilds.get('219256995574710272').members.size();
-	return message.reply(${memberCount}); 
+	
+	return message.reply('Somos monos.'); 
  }
 
 });
