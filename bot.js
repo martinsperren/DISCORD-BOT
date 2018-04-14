1a32b9e16eea7728b2e0f49eede0e3ba4c70a51b
Martin Perren <martinperren@gmail.com>
2018-04-14 06:14:56 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -527,7 +527,7 @@ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
   var mins = Math.round((client.uptime % 3.6e6) / 6e4);	
 
 		
-message.channel.send(`${days} dias ${hrs} horas ${mins} minutos`); 	
+message.channel.send(`UPTIME: ${days} DIAS ${hrs} HS ${mins} MINS`); 	
 		
 		
 		
