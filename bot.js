c6958fc5463850554496599780adb1a948991c7d
Martin Perren <martinperren@gmail.com>
2018-05-25 04:19:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,15 +17,15 @@ const queue = new Map();
 
 client.on("ready", () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-client.user.setGame(`VIVA LA PATRIA`);	
+client.user.setGame(process.env.GAME);	
 });
 client.on("guildCreate", guild => {
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setGame(`VIVA LA PATRIA`);		
+    client.user.setGame(process.env.GAME);		
 });
 client.on("guildDelete", guild => {
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
- client.user.setGame(`VIVA LA PATRIA`);		
+ client.user.setGame(process.env.GAME);		
 });
 client.on('guildMemberAdd', member => {
     member.guild.channels.get('219256995574710272').send('**' + member.user.username + '** ahora vive en el bunker! :house:');
